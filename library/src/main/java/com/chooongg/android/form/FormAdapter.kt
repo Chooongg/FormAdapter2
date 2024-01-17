b8f78e5af7fa5956d3cde64ee00bcd32a45528d0
Chooongg <chooongg@outlook.com>
2024-01-17 17:38:59 +0800

    Initial commit

--- library/src/main/java/com/chooongg/android/form/FormAdapter.kt
+++ library/src/main/java/com/chooongg/android/form/FormAdapter.kt
@@ -2,17 +2,13 @@ package com.chooongg.android.form
 
 import androidx.recyclerview.widget.RecyclerView
 import com.chooongg.android.form.data.FormPartData
-import com.chooongg.android.form.item.BaseForm
 import com.chooongg.android.form.listener.FormOnMenuItemClickListener
 import com.chooongg.android.form.part.AbstractPart
 import com.chooongg.android.form.part.FormPart
 import com.chooongg.android.form.style.AbstractStyle
 import com.chooongg.android.form.style.EmptyStyle
 
-class FormAdapter(
-    isEnabled: Boolean = false,
-    val default: FormManager.Default = FormManager.default
-) : AbstractFormAdapter() {
+class FormAdapter(isEnabled: Boolean = false) : AbstractFormAdapter() {
 
     val formPool = RecyclerView.RecycledViewPool()
 
